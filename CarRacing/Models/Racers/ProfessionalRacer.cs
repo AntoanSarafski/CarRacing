aa85d4581d198cff8e77f02528c63646ff1ab84d
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-12 15:42:59 +0300

    Commit with child class for Racers - ProfessionalRacer with constructor.

---
+++ CarRacing/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,17 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public class ProfessionalRacer : Racer
+    {
+        private const int _drivingExperience = 30;
+        private const string _racingBehavior = "strict"; 
+        public ProfessionalRacer(string username, ICar car) 
+            : base(username, _racingBehavior, _drivingExperience, car)
+        {
+        }
+    }
+}
