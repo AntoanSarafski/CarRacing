2646447b5e6e18de7d3fb69403b37a9814c08e0e
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-12 15:43:59 +0300

    Commit with child class for Racers - StreetRacer with constructor.

---
+++ CarRacing/Models/Racers/StreetRacer.cs
@@ -0,0 +1,17 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public class StreetRacer : Racer
+    {
+        private const int _drivingExperience = 10;
+        private const string _racingBehavior = "agressive";
+        public StreetRacer(string username, ICar car)
+            : base(username, _racingBehavior, _drivingExperience, car)
+        {
+        }
+    }
+}
