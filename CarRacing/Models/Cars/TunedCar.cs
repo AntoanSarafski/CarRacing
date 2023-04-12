e2cab7a50df96c2d165ce745c8bc8458d6a7c791
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-12 15:34:38 +0300

    Commit with the other Child class - TunedCar.

---
+++ CarRacing/Models/Cars/TunedCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Cars
+{
+    public class TunedCar : Car
+    {
+        private const int _fuelAvailable = 65;
+        private const double _fuelConsumptionPerRace = 7.5;
+        public TunedCar(string make, string model, string vIN, int horsePower)
+            : base(make, model, vIN, horsePower, _fuelAvailable, _fuelConsumptionPerRace)
+        {
+        }
+    }
+}
