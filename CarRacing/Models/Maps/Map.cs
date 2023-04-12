6e4915ca707fe4f2cd7445682b840d257abcfe86
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-12 23:06:54 +0300

    Commit with finished skeleton.

--- CarRacing/Models/Maps/Map.cs
+++ CarRacing/Models/Maps/Map.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using CarRacing.Models.Maps.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CarRacing.Models.Maps
 {
-    internal class Map
+    public class Map : IMap
     {
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
     }
 }
--- CarRacing/Repositories/RacerRepository.cs
+++ CarRacing/Repositories/RacerRepository.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using CarRacing.Repositories.Contracts;
+using CarRacing.Utilities.Messages;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CarRacing.Repositories
 {
-    internal class RacerRepository
+    public class RacerReposirory : IRepository<IRacer>
     {
+        private readonly List<IRacer> racers;
+
+        public IReadOnlyCollection<IRacer> Models => racers.AsReadOnly();
+
+        public void Add(IRacer racer)
+        {
+            if (racer == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidAddRacerRepository);
+            }
+        }
+
+        public bool Remove(IRacer racer)
+        {
+            if (racers.Remove(racer))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public IRacer FindBy(string property)
+        {
+
+            if (racers.FirstOrDefault(r => r.Username == property) != null)
+            {
+                return racers.First(r => r.Username == property);
+            }
+            return null;
+        }
+
+
     }
 }
