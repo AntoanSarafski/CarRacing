2e64cadbf05d2ff27707dddcef91e179e6efdb96
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-12 22:49:37 +0300

    Commit with CarRepository class.

---
+++ CarRacing/Repositories/CarRepository.cs
@@ -0,0 +1,46 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Repositories.Contracts;
+using CarRacing.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarRacing.Repositories
+{
+    public class CarRepository : IRepository<ICar>
+    {
+        private readonly List<ICar> cars;
+        public IReadOnlyCollection<ICar> Models => cars.AsReadOnly();
+
+        public void Add(ICar car)
+        {
+            if(car == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidAddCarRepository);
+            }
+        }
+
+        public bool Remove(ICar car)
+        {
+            if (cars.Remove(car))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public ICar FindBy(string property)
+        {
+            
+            if (cars.FirstOrDefault(c => c.VIN == property) == null)
+            {
+                return cars.First(c => c.VIN == property);
+            }
+            return null;
+        }
+
+
+    }
+}
