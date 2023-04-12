e4647c99db0d9542d87031d904d6da4708be364d
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-12 15:39:57 +0300

    Commit with Racer class with all properties validations.

---
+++ CarRacing/Models/Racers/Racer.cs
@@ -0,0 +1,81 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using CarRacing.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public abstract class Racer : IRacer
+    {
+        private string username;
+        private string racingBehavior;
+        private int drivingExperience;
+        private ICar car;
+
+        public string Username 
+        { 
+            get => username;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerName);
+                }
+                username = value;
+            }
+        }
+
+        public string RacingBehavior 
+        {
+            get => racingBehavior;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerBehavior);
+                }
+                racingBehavior = value;
+            }
+        }
+
+        public int DrivingExperience 
+        { 
+            get => drivingExperience;
+            private set
+            {
+                if(value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerDrivingExperience);
+                }
+                drivingExperience = value;
+            }
+        }
+
+        public ICar Car 
+        { 
+            get => car;
+            private set
+            {
+                if(car == null)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerCar);
+                }
+                car = value;
+            } 
+        }
+
+        public bool IsAvailable()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void Race()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
